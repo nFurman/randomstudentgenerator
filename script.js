d77df5b11a0267489924b015ed31d55f044bf547
nFurman <115150157+nFurman@users.noreply.github.com>
2022-11-10 11:18:31 +0200

    fixed shadows, fixed home button, changed names

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 let arrStudent = [
   "Daniel",
-  "Samuel",
+  "Sammy",
   "Esther",
-  "Zachary",
+  "Zach",
   "David",
   "Yonatan",
   "Jonah",
-  "Nicholas",
+  "Nicky",
   "Tyler",
   "Marko",
   "EZ",
   "James",
-  "Moises",
+  "Moi",
   "Yair",
-  "Ezekiel",
+  "Zeke",
   "Colton",
   "Natan",
   "Yohan",
@@ -26,69 +26,66 @@ for (let i = 0; i < arrStudent.length; i++) {
   y.setAttribute("type", "checkbox");
   y.setAttribute("name", "student");
   y.setAttribute("value", arrStudent[i]);
-  y.setAttribute("id", "checkbox");
+  y.setAttribute("id", i);
   var z = document.createElement("label");
-  z.setAttribute("for", arrStudent[i]);
+  z.setAttribute("for", i);
   form.appendChild(y);
   form.appendChild(z);
-  z.setAttribute("id", "student");
+  z.setAttribute("class", "studentLabel");
   z.appendChild(document.createTextNode(arrStudent[i]));
   var a = document.createElement("br");
   form.appendChild(a);
 }
 
-let checkedstudents = [];
+let validStudents = [];
+
+const randomStudent = () => {
+  let randomNum = Math.floor(Math.random() * validStudents.length);
+  return validStudents[randomNum];
+};
 
 function myFunction() {
   document.getElementById("body").style.display = "none";
+  validStudents = [];
   for (let i = 0; i < arrStudent.length; i++) {
-    if (document.querySelector(`input[value=${arrStudent[i]}]:checked`)) {
-      checkedstudents.push(i);
+    if (document.getElementById(i).checked === false) {
+      console.log(arrStudent[i]);
+      validStudents.push(arrStudent[i]);
     }
   }
-  let reversedchecked = checkedstudents.reverse();
-
-  for (let j = 0; j < reversedchecked.length; ) {
-    arrStudent.splice(reversedchecked[j], 1);
-    reversedchecked.splice(j, 1);
-  }
-  let randomStudent = () => {
-    randomNum = Math.floor(Math.random() * arrStudent.length);
-    return arrStudent[randomNum];
-  };
 
   document.getElementById("countdown").style.display = "block";
   document.getElementById("countnumber").style.display = "block";
   document.getElementById("countdown").innerHTML =
     "The random student will be revealed in:";
 
-  let count = 5;
-  document.getElementById("countnumber").innerHTML = count;
+  countdown(5);
+}
 
-  setInterval(countdown, 1000);
+function revealnewstudent() {
+  randomStudent();
+  document.getElementById("randomstudentis").innerHTML =
+    "The Random Student is:";
+  document.getElementById("studentname").innerHTML = randomStudent();
+}
 
-  function countdown() {
-    if (count > 1) {
-      count--;
-      document.getElementById("countnumber").innerHTML = count;
-    } else if (count > 0) {
-      document.getElementById("countdown").style.display = "none";
-      document.getElementById("countnumber").style.display = "none";
-      document.getElementById("randomstudentis").style.display = "block";
-      document.getElementById("studentname").style.display = "block";
-      document.getElementById("button3").style.display = "block";
-    }
+function countdown(count) {
+  if (count > 0) {
+    document.getElementById("countnumber").innerHTML = count;
+    setTimeout(countdown, 1000, count - 1);
+  } else {
+    revealnewstudent();
+    document.getElementById("countdown").style.display = "none";
+    document.getElementById("countnumber").style.display = "none";
+    document.getElementById("randomstudentis").style.display = "block";
+    document.getElementById("studentname").style.display = "block";
+    document.getElementById("button3").style.display = "block";
   }
-
-  function revealnewstudent() {
-    randomStudent();
-    document.getElementById("randomstudentis").innerHTML =
-      "The Random Student is:";
-    document.getElementById("studentname").innerHTML = randomStudent();
-  }
-  revealnewstudent();
 }
 
 function home() {
-  location.reload();
+  document.getElementById("body").style.display = "block";
+  document.getElementById("randomstudentis").style.display = "none";
+  document.getElementById("studentname").style.display = "none";
+  document.getElementById("button3").style.display = "none";
 }
